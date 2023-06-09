e784cc69d5493d85c8d1df84cfbe76eea309af78
RayanHamadeh <rayanhamadeh10@gmail.com>
2023-06-09 11:08:11 -0400

    added info

---
+++ about.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>About Me</title>
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+    <button><a href="index.php">Home Page</a></button>
+    <button><a href="about.php">About Me</a></button>
+  <h1>About Me</h1>
+
+  <div class="section">
+    <h2>Introduction</h2>
+    <p>Hi, my name is Rayan Hamadeeh. I'm a student in Vanier College studying Website design. I love creating beautiful and interactive user interfaces using HTML, CSS, and JavaScript.</p>
+  </div>
+
+  <div class="section">
+    <h2>Skills</h2>
+    <p>I have basic experience with HTML, CSS, JavaScript, and responsive web design.</p>
+  </div>
+
+
+  <div class="section">
+    <h2>Education</h2>
+    <p>I graduated with a Bachelor's degree in Accounting Information System </p>
+  </div>
+
+  <div class="section">
+    <h2>Interests</h2>
+    <p>When I'm not coding, I enjoy reading tech blogs, attending tech conferences, and exploring new technologies. I believe in continuous learning and staying up-to-date with the latest industry trends.</p>
+  </div>
+
+  <div class="section">
+    <h2>Contact</h2>
+    <p>If you would like to get in touch with me, you can reach me at rayanhamadeh10@gmail.com. I'm open to new opportunities and collaborations, so feel free to reach out!</p>
+  </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+    <button><a href="index.php">Home Page</a></button>
+    <button><a href="about.php">About Me</a></button> 
+  <header>
+    <h1>About Me</h1>
+  </header>
+  <div class="container">
+    <section>
+      <h2>Introduction</h2>
+      <p>Hi, my name is Rayan Hamadeh. I'm a student in Vanier College studying Website design. I love creating beautiful and interactive user interfaces using HTML, CSS, and JavaScript.</p>
+    </section>
+  </div>
+</body>
+</html>
+
+
